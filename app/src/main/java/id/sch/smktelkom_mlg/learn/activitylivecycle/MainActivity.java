784de786c4e4ad3c76605e34442a34100643c265
belabonga <shania1faza@gmail.com>
2016-11-04 11:04:57 +0700

    Livecycle Basic

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -2,12 +2,57 @@ package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "LivecycleTag";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart : ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume : ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause : ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onPause();
+        Log.d(TAG, "onStop : ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onPause();
+        Log.d(TAG, "onRestart : ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onPause();
+        Log.d(TAG, "onDestroy : ");
+    }
+
 }
